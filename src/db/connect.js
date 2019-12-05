fca3d9d6d4796a867884d9ff8a6e89204339f686
kasule joseph <joseph.kasule@andela.com>
2019-12-05 16:10:51 +0200

    connect to cashcog db

---
+++ src/db/connect.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config()
+let connectionString
+
+process.env.NODE_ENV == 'local' ? connectionString = process.env.DBLOCAL : connectionString = process.env.DBPROD
+
+mongoose.connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+
+}).catch((error) => console.log(error))
