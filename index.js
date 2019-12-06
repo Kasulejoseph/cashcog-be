a1f701c32a18a1deee4cdd1485736199b976b4f9
kasule joseph <joseph.kasule@andela.com>
2019-12-06 10:18:40 +0200

    expense stream [Delivers #170127223]

--- index.js
+++ index.js
@@ -1,24 +1,14 @@
 import express from 'express'
-import request from 'request'
-import es from 'event-stream'
-
 import './src/db/connect'
 const app = express()
 const router = express.Router()
-
-request.get('https://cashcog.xcnt.io/stream')
-    .pipe(es.split())
-    .pipe(es.map(function (line, cb) {
-        let yy = JSON.parse(line)
-        console.log(yy.uuid);
-        
-        cb(null, line)
-    }))
-
+import expenseStream from './src/helper/expenseStream'
+expenseStream()
 router.get('/', (req, res) => {
     res.send({
         message: 'me you'
     })
 })
+
 app.use(router)
 app.listen(4000)
