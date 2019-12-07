2875693c4eff6b4a641b6d2b776483cf62e74574
kasule joseph <joseph.kasule@andela.com>
2019-12-07 13:00:23 +0200

    fetch user expenses

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const app = express()
 const router = express.Router()
 import ExpenseController from './src/controller'
 import expenseStream from './src/helper/expenseStream'
-
+const { getExpenses, userExpenses } = ExpenseController
 // expenseStream()
-router.get('/', ExpenseController.getExpenses)
+router.get('/', getExpenses)
+router.get('/:id', userExpenses)
 
 app.use(router)
 app.listen(3000)
--- src/controller/index.js
+++ src/controller/index.js
@@ -1,9 +1,20 @@
 import Expense from '../model/expense'
+import User from '../model/user';
 class ExpenseController {
     static async getExpenses(req, res) {
         const data = await Expense.find({}).populate('users')
         res.send({
             status: 200,
+            data
+        })
+    }
+    static async userExpenses(req, res) {
+        const { id } = req.params
+        const data = await Expense.find({employee: id}).populate('users')
+        const employee = await User.find({uuid: id})
+        res.send({
+            status: 200,
+            employee,
             data
         })
     }
