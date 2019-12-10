49007e03b74b5465582beb1766e8a14cb919972b
kasulejoseph <kasule06joseph@gmail.com>
2019-12-10 22:55:01 +0300

    stream on active server

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
-import express from 'express'
-import cors from 'cors'
-import './src/db/connect'
+import express from "express";
+import cors from "cors";
+import "./src/db/connect";
 
-const app = express()
+const app = express();
 
-import expenseStream from './src/helper/expenseStream'
-import routers from './src/routers'
-// expenseStream()
-app.use(express.json())
-app.use(cors())
-app.use(routers)
+import expenseStream from "./src/helper/expenseStream";
+import routers from "./src/routers";
 
-export default app
+// to be improve : have it's own script probably..
+expenseStream();
+
+app.use(express.json());
+app.use(cors());
+app.use(routers);
+
+export default app;
--- src/db/connect.js
+++ src/db/connect.js
@@ -7,9 +7,10 @@ process.env.NODE_ENV == "local"
   ? (connectionString = process.env.DBLOCAL)
   : (connectionString = process.env.DBPROD);
 
-  if(process.env.NODE_ENV == "test") {
-    connectionString = "mongodb://cashcog:casjos06@cluster0-shard-00-00-ac86u.mongodb.net:27017,cluster0-shard-00-01-ac86u.mongodb.net:27017,cluster0-shard-00-02-ac86u.mongodb.net:27017/cashcog-tests?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority"
-  }
+if (process.env.NODE_ENV == "test") {
+  connectionString =
+    "mongodb://cashcog:casjos06@cluster0-shard-00-00-ac86u.mongodb.net:27017,cluster0-shard-00-01-ac86u.mongodb.net:27017,cluster0-shard-00-02-ac86u.mongodb.net:27017/cashcog-tests?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority";
+}
 
 mongoose
   .connect(connectionString, {
